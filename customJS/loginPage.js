7f3ea046f513212cf6d73760d45f4859df583bb2
JuwelShaikh <juwelshaikh04@gmail.com>
2023-09-02 14:08:55 +0600

    Added home page

--- customJS/loginPage.js
+++ customJS/loginPage.js
@@ -3,5 +3,15 @@ document.getElementById('btn-logIn').addEventListener('click', function(){
 
     const userField = document.getElementById('user-id');
     const email = userField.value;
-    console.log(email);
+    
+    const passwordField = document.getElementById('pass=field');
+    const pass = passwordField.value;
+    
+
+    if(email === 'juwel@gmail.com' && pass == '12345juwel'){
+        alert('Submitted successful.');
+    }
+    else{
+        alert('Wrong info.')
+    }
 })
