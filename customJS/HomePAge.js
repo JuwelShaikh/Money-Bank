4bcbc8b1a84bb84639905cc9fde769e0a31c25da
JuwelShaikh <juwelshaikh04@gmail.com>
2023-09-05 00:26:28 +0600

    Diposit connect with JS

--- customJS/HomePAge.js
+++ customJS/HomePAge.js
@@ -1,1 +1,35 @@
-alert('kire?');
+
+// input field of Diposit
+document.getElementById('btn-diposit').addEventListener('click',function(){
+    const inputFieldOne = document.getElementById('input-id-one');
+    const newDipositString = inputFieldOne.value;
+    const newDiposit = parseFloat(newDipositString);
+    
+    const dipositElement = document.getElementById('diposit');
+    const previousDipositString = dipositElement.innerText;
+    const previousDiposit = parseFloat(previousDipositString);
+    
+    const currentDiposit = previousDiposit + newDiposit;
+    
+    dipositElement.innerText = currentDiposit;
+
+    inputFieldOne.value = '';
+
+})
+
+// input field of Withdraw
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    const inputFieldTwo = document.getElementById('input-id-two');
+    const newWithdrawalString = inputFieldTwo.value;
+    const newWithdraw = parseFloat(newWithdrawalString);
+    
+    const withdrawElement = document.getElementById('withdraw');
+    const previousWithdrawalString = withdrawElement.innerText;
+    const previousWithdrawal = parseFloat(previousWithdrawalString);
+
+    const currentWithdraw = previousWithdrawal + newWithdraw;
+
+    withdrawElement.innerText = currentWithdraw;
+    
+    inputFieldTwo.value = '';
+})
