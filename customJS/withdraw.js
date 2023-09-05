bea979caaab75b5d375e6230a0658de16379e95c
JuwelShaikh <juwelshaikh04@gmail.com>
2023-09-05 23:09:14 +0600

    Withdraw connect with balanced

--- customJS/withdraw.js
+++ customJS/withdraw.js
@@ -1,7 +1,24 @@
 
 document.getElementById('btn-withdraw').addEventListener('click',function(){
     const withdrawInputField = document.getElementById('withdraw-input-field');
-    const withdraw = withdrawInputField.value;
-    
+    const newWithdrawString = withdrawInputField.value;
+    const newWithdraw = parseFloat(newWithdrawString);
 
+    const withdrawAmmount = document.getElementById('withdraw-ammount');
+    const previousWithdrawString = withdrawAmmount.innerText;
+    const previousWithdraw = parseFloat(previousWithdrawString);
+    
+    const currentWithdraw = previousWithdraw + newWithdraw;
+    withdrawAmmount.innerText = currentWithdraw;
+
+    // balance
+    const balanceField = document.getElementById('balance-field');
+    const previousBalanceString = balanceField.innerText;
+    const previousBalance = parseFloat(previousBalanceString);
+    
+    const currentBalance = previousBalance - newWithdraw;
+
+    balanceField.innerText = currentBalance;
+    
+    withdrawInputField.value = '';
 })
