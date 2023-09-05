ecf114eff91a096204a3f9b6de3eb1efcc9c3dd1
JuwelShaikh <juwelshaikh04@gmail.com>
2023-09-05 22:24:31 +0600

    withdraw added

---
+++ customJS/withdraw.js
@@ -0,0 +1,7 @@
+
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    const withdrawInputField = document.getElementById('withdraw-input-field');
+    const withdraw = withdrawInputField.value;
+    
+
+})
